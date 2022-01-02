2642668cb18740290ded65624daaac0bfd39ce59
Marius C. Bolik <marius@condacore.com>
2022-01-02 15:48:44 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cleverpush',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
